1331705a2d423a85bf13bf8987ad8ec9f2b44a3a
Alek30k <alemank23k@gmail.com>
2023-10-12 23:25:14 -0300

    last commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,14 +3,19 @@ import "./hero.scss";
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="textContainer">
-        <h2>HARVEY TYLER</h2>
-        <h1> Web developer and UI designer</h1>
-        <div className="buttons">
-          <button>See the Latest Works</button>
-          <button>Contact Me</button>
+      <div className="wrapper">
+        <div className="textContainer">
+          <h2>ALEJANDRO CABRERA</h2>
+          <h1>Full Stack Developer</h1>
+          <div className="buttons">
+            <button>See the Latest Works</button>
+            <button>Contact Me</button>
+          </div>
+          <img src="/scroll.png" alt="" />
         </div>
-        <img src="/scroll.png" alt="" />
+      </div>
+      <div className="slidingTextContainer">
+        Writer Content Creator Influencer
       </div>
       <div className="imageContainer">
         <img src="/hero.png" alt="" />
