d3be57fb35756589ab7b87ca013d4a7d9eb939a4
Alek30k <alemank23k@gmail.com>
2023-10-13 22:55:48 -0300

    last commit

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,7 +1,14 @@
 import "./parallax.scss";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-const Parallax = () => {
-  return <div className="parallax">Parallax</div>;
+const Parallax = ({ type }) => {
+  return (
+    <div className="parallax">
+      <motion.h1>
+        {type === "services" ? "What We Do?" : "What We Did?"}
+      </motion.h1>
+    </div>
+  );
 };
 
 export default Parallax;
