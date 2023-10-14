44bc30f34190ebc50240d83cadbb37408e03e85f
Alek30k <alemank23k@gmail.com>
2023-10-13 23:16:38 -0300

    last commit

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -10,6 +10,9 @@ const Parallax = ({ type }) => {
     offset: ["start start", "end start"],
   });
 
+  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "500%"]);
+  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+
   return (
     <div
       className="parallax"
