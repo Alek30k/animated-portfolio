958397dd039535a34a7a6de5e0a7236b29fa61e3
Alek30k <alemank23k@gmail.com>
2023-10-16 16:37:50 -0300

    last commit

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -27,7 +27,18 @@ const Services = () => {
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-      <motion.div className="listContainer"></motion.div>
+      <motion.div className="listContainer">
+        <div className="box">
+          <h2>Branding</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
+          </p>
+          <button>Go</button>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
