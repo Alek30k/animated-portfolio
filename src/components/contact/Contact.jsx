a0e41dff6d0ba9744b4ff71680d40a384d507440
Alek30k <alemank23k@gmail.com>
2023-10-17 17:20:22 -0300

    last commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -29,10 +29,10 @@ const Contact = () => {
 
     emailjs
       .sendForm(
-        "service_94y20xo",
-        "template_v10u2oh",
+        "service_1qa9xko",
+        "template_r8d7veg",
         formRef.current,
-        "pX_2hasGmGcuvjXIW"
+        "2vSQeLwIQGjKVLfFf"
       )
       .then(
         (result) => {
