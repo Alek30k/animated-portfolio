735e1c60c60d44862407bbd0052a3986861dd6f4
Alek30k <alemank23k@gmail.com>
2023-10-17 16:23:16 -0300

    last commit

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,29 @@
 import "./contact.scss";
 
 const Contact = () => {
-  return <div className="contact">Contact</div>;
+  return (
+    <div className="contact">
+      <div className="textContainer">
+        <h1>Let’s work together</h1>
+        <div className="item">
+          <h2>Mail</h2> <span>alemank23k@gmail.com</span>
+        </div>
+        <div className="item">
+          <h2>Address</h2>{" "}
+          <span>Argentina, Formosa, General Manuel Belgrano</span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2> <span>+54 3704-678598</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
